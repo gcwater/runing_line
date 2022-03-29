e35e789d685840771c693e57567f65e1326c08df
Nadesu Rajasurendran <86802215+gcwater@users.noreply.github.com>
2022-03-29 14:23:21 -0500

    Add files via upload

---
+++ testing.js
@@ -0,0 +1,68 @@
+<script>
+  require([
+  "esri/Map",
+  "esri/views/MapView",
+  "esri/layers/FeatureLayer",
+  "esri/widgets/Feature"
+], function(ArcGISMap, MapView, FeatureLayer, Feature) {
+  const tooltip = document.getElementById("tooltip");
+
+  const layer = new FeatureLayer({
+    portalItem: {
+      id: "04482d030fd7430f9c35382b457c605d"
+    },
+    outFields: ["*"],
+    popupEnabled: false
+  });
+
+  const map = new ArcGISMap({
+    layers: [layer],
+    basemap: "gray-vector"
+  });
+  const view = new MapView({
+    container: "viewDiv",
+    map: map,
+    center: [-95.4, 29.4],
+          zoom: 10
+  });
+
+  const featureWidget = new Feature({ view, container: tooltip });
+  
+  let layerView;
+  
+  view.whenLayerView(layer).then(result => layerView = result);
+
+  view.on("pointer-move", event => {
+    const { x, y } = event;
+    view.hitTest(event).then(({ results }) => {
+      if (layerView) {
+        layerView.effect = null;
+      }
+      if (results.length) {
+        tooltip.style.display = "block";
+        tooltip.style.top = `${y - 175}px`;
+        tooltip.style.left = `${x - 310/2}px`;
+        const g = results[0].graphic;
+        if (g.geometry) {
+          const oid = g.attributes.OBJECTID;
+          featureWidget.graphic = g;
+          if (layerView) {
+            layerView.effect = {
+              filter: {
+                where: `OBJECTID = ${oid}`
+              },
+              excludedEffect: "opacity(20%) saturate(30%)",
+              // includedEffect: "sepia(50%)"
+            };
+          }
+        } else {
+          tooltip.style.display = "none";
+        }
+      } else {
+        tooltip.style.display = "none";
+      }
+    });
+  });
+});
+
+    </script>
